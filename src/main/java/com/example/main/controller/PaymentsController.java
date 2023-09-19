f0803c4e3e38af8b92a7e55c30dbcbc73520a8b6
Boris2514 <borismiashiro@gmail.com>
2023-09-18 22:01:54 -0400

    Updating Doc :V

---
+++ src/main/java/com/example/main/controller/PaymentsController.java
@@ -0,0 +1,19 @@
+package com.example.main.controller;
+
+import com.example.main.model.Payment;
+import com.example.main.proxy.PaymentsProxy;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentsController {
+  private final PaymentsProxy paymentsProxy;
+
+  public PaymentsController(PaymentsProxy paymentsProxy) {this.paymentsProxy = paymentsProxy;}
+
+  @PostMapping("/payment")
+  public Payment createPayment(@RequestBody Payment payment) {
+    return paymentsProxy.createPayment(payment);
+  }
+}
